2a350f635e051ff9c0d377e1fb16412cdacb855d
drhuangwc <42527445+drhuangwc@users.noreply.github.com>
2023-10-05 11:43:31 +0800

    env setup

---
+++ python_env_setup.sh
@@ -0,0 +1,20 @@
+python3 -m pip install -U pip
+
+pip3 install numpy pandas matplotlib beautifulsoup4 
+pip3 install jupyter notebook scipy ipython sympy nose openpyxl xlsxwriter
+pip3 install -U "setuptools==59.5.0"
+pip3 install -U wheel 
+pip3 install bitarray
+pip3 install -U "scikit-learn<0.25"
+pip3 install reportlab
+pip3 install "pyqt5<5.13"
+pip3 install "pyqtwebengine<5.13"
+
+pip3 install -U "mxnet<2.0.0, >=1.7.0"
+
+echo "------- install autogluon --"
+
+pip3 install autogluon
+pip3 install torch torchvision torchaudio
+pip3 install rdkit-pypi
+pip3 install mrmr_selection
