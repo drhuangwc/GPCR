74b92d994c30db0de0e67e8c39a4a720c7f17ac2
Your Name <drhuangwc@gmail.com>
2023-10-06 14:37:05 +0800

    upload

---
+++ extract_files1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+
+cd Human_AgonistAntagonist
+tar -xvf GPCR_AgonistAntagonist20220915.tar.xz
+tar -xvf GPCR_AgonistAntagonist_protein20220915.tar.xz
+tar -xvf GPCR_AgonistAntagonist_protein_align_ptcode123_20220915.tar.xz
+
+cd GPCR_similarty
+tar -xvf GPCR_to_trainAgonistAntagonist_humanalltrainconcat_selefeatureTOP200.tar.xz
+tar -xvf similarty_proteinpairsimilarity20230624_TOP200_proteinseq20220915_align_7CUT.tar.xz
+
+
+
+
+
