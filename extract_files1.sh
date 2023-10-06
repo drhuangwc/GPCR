3bccddd17feda431da3e2ddb285f45c680ab5292
Your Name <drhuangwc@gmail.com>
2023-10-06 14:57:43 +0800

    upload

--- extract_files1.sh
+++ extract_files1.sh
@@ -5,6 +5,17 @@ tar -xvf GPCR_AgonistAntagonist20220915.tar.xz
 tar -xvf GPCR_AgonistAntagonist_protein20220915.tar.xz
 tar -xvf GPCR_AgonistAntagonist_protein_align_ptcode123_20220915.tar.xz
 
+
+cat GPCR_AgonistAntagonist_protein_align_ptcode123_padel1d2d20220921_rdkECFP1019.tar.xz.part* > GPCR_AgonistAntagonist_protein_align_ptcode123_padel1d2d20220921_rdkECFP1019.tar.xz
+
+tar -xvf GPCR_AgonistAntagonist_protein_align_ptcode123_padel1d2d20220921_rdkECFP1019.tar.xz
+
+cat padleout_molecule_1d2ddescriptor09190015_GPCR_AgonistAntagonist_protein_align_ptcode123_20220915.tar.xz.part* > padleout_molecule_1d2ddescriptor09190015_GPCR_AgonistAntagonist_protein_align_ptcode123_20220915.tar.xz
+
+tar -xvf padleout_molecule_1d2ddescriptor09190015_GPCR_AgonistAntagonist_protein_align_ptcode123_20220915.tar.xz
+
+
+
 cd GPCR_similarty
 tar -xvf GPCR_to_trainAgonistAntagonist_humanalltrainconcat_selefeatureTOP200.tar.xz
 tar -xvf similarty_proteinpairsimilarity20230624_TOP200_proteinseq20220915_align_7CUT.tar.xz
