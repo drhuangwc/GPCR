026ec5c6ed029c84b7534774befc1da142f057aa
Your Name <drhuangwc@gmail.com>
2023-10-06 15:03:09 +0800

    upload

--- extract_files1.sh
+++ extract_files1.sh
@@ -5,15 +5,14 @@ tar -xvf GPCR_AgonistAntagonist20220915.tar.xz
 tar -xvf GPCR_AgonistAntagonist_protein20220915.tar.xz
 tar -xvf GPCR_AgonistAntagonist_protein_align_ptcode123_20220915.tar.xz
 
-
-cat GPCR_AgonistAntagonist_protein_align_ptcode123_padel1d2d20220921_rdkECFP1019.tar.xz.part* > GPCR_AgonistAntagonist_protein_align_ptcode123_padel1d2d20220921_rdkECFP1019.tar.xz
-
+cat GPCR_AgonistAntagonist_protein_align_ptcode123_padel1d2d20220921_rdkECFP1019.tar.xz.part* > GPCR_AgonistAntagonist_protein_align_ptcode123_padel1d2d20220921_rdkECFP1019.tar.xz 
 tar -xvf GPCR_AgonistAntagonist_protein_align_ptcode123_padel1d2d20220921_rdkECFP1019.tar.xz
 
+cat GPCR_to_trainAgonistAntagonist_all.tar.xz.part* > GPCR_to_trainAgonistAntagonist_all.tar.xz 
+tar -xvf GPCR_to_trainAgonistAntagonist_all.tar.xz
+
 cat padleout_molecule_1d2ddescriptor09190015_GPCR_AgonistAntagonist_protein_align_ptcode123_20220915.tar.xz.part* > padleout_molecule_1d2ddescriptor09190015_GPCR_AgonistAntagonist_protein_align_ptcode123_20220915.tar.xz
-
 tar -xvf padleout_molecule_1d2ddescriptor09190015_GPCR_AgonistAntagonist_protein_align_ptcode123_20220915.tar.xz
-
 
 
 cd GPCR_similarty
